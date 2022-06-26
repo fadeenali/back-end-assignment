2bd3bde0f707d86511f6c77eaec404a916732355
fardeenali <fardeenali1602@gmail.com>
2022-06-26 23:57:48 +0530

    Added a Procfile.

--- index.js
+++ index.js
@@ -6,18 +6,17 @@ const app = express();
 const port = 5000;
 app.use(bodyParser.json());
 
-
-// Routes 
+// Routes
 app.use("/student", Student_Controller);
 
-
-
-// DB connection and server start-up 
+// DB connection and server start-up
 const main = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/myassignment").then(() => {
-      console.log("DB is connected");
-    });
+    await mongoose
+      .connect("mongodb://localhost:27017/myassignment")
+      .then(() => {
+        console.log("DB is connected");
+      });
 
     app.listen(port, () => {
       console.log(`server is up and runnin on ${port}`);
